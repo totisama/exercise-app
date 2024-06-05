c3ca035aa031503747fdb141eae1e3c937a597d9
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-06-05 20:33:49 +0200

    WIP: Display exercise

--- src/app/exercise/[id]/page.tsx
+++ src/app/exercise/[id]/page.tsx
@@ -1,3 +1,41 @@
-export default function ExercisePage() {
-  return <h1>ExercisePage</h1>
+import { Back } from '@/components/back'
+import { sdk } from '@/lib/client'
+import Image from 'next/image'
+
+export default async function ExercisePage({
+  params: { id },
+}: {
+  params: { id: string }
+}) {
+  const { data } = await sdk.GetExercise({ id })
+  const { exercise } = data
+
+  if (exercise === null || exercise === undefined) {
+    return <h1>Exercise not found</h1>
+  }
+
+  return (
+    <main className='w-full mt-5 px-5 pb-10'>
+      <Back />
+      <section className='space-y-3'>
+        <div className='w-full flex flex-col items-center'>
+          <Image
+            src={exercise.image.url}
+            alt='exercise'
+            width={500}
+            height={500}
+            className='rounded-3xl'
+          />
+        </div>
+        <div className='space-y-2'>
+          <h1 className='text-4xl font-bold text-center'>{exercise.name}</h1>
+          <p>{exercise.description}</p>
+        </div>
+        <div>
+          <h2 className='text-3xl font-semibold'>Instructions</h2>
+          <p>{exercise.instructions}</p>
+        </div>
+      </section>
+    </main>
+  )
 }
