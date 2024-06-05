98981af3d258f85a68fd56a56209ce5896fe3d7f
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-06-05 11:43:39 +0200

    customize header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,16 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { WeightIcon } from '@/icons'
+import Link from 'next/link'
 
 export const Header = () => {
   return (
-    <header className='flex justify-between border-b border-black py-2 px-3'>
-      <h1 className='text-2xl font-bold'>Exercise App</h1>
-      <Avatar className='h-8 w-8'>
+    <header className='flex justify-between items-center border-b border-black py-2 px-3'>
+      <Link href='/'>
+        <h2 className='flex items-center text-3xl font-bold'>
+          Exercise <WeightIcon /> App
+        </h2>
+      </Link>
+      <Avatar className='h-10 w-10'>
         <AvatarImage src={'https://github.com/shadcn.png'} />
         <AvatarFallback>Toti</AvatarFallback>
       </Avatar>
