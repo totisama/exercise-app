585700bc2a5335603dad945e5ed70ff434b77c86
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-06-05 17:44:21 +0200

    add icon

--- src/icons.tsx
+++ src/icons.tsx
@@ -70,3 +70,21 @@ export const ArrowUpIcon = ({ className }: { className?: string }) => (
     />
   </svg>
 )
+
+export const ArrowLeftIcon = ({ className }: { className?: string }) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='800'
+    height='800'
+    fill='none'
+    viewBox='0 0 24 24'
+    className={className}
+  >
+    <path
+      fill='#000'
+      fillRule='evenodd'
+      d='M11.707 4.293a1 1 0 0 1 0 1.414L6.414 11H20a1 1 0 1 1 0 2H6.414l5.293 5.293a1 1 0 0 1-1.414 1.414l-7-7a1 1 0 0 1 0-1.414l7-7a1 1 0 0 1 1.414 0Z'
+      clipRule='evenodd'
+    />
+  </svg>
+)
