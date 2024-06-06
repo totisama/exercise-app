841046e4634aa9a4d99bdc20e52f4180f33584f1
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-06-06 18:32:46 +0200

    Create useMusic hook

---
+++ src/hooks/useMusic.ts
@@ -0,0 +1,24 @@
+import { useRef } from 'react'
+
+export const useMusic = () => {
+  const audioRef = useRef<HTMLAudioElement>(null)
+
+  const play = () => {
+    audioRef.current?.play()
+    setVolume(0.25)
+  }
+
+  const stop = () => {
+    audioRef.current?.pause()
+  }
+
+  const setVolume = (volume: number) => {
+    if (audioRef.current === null) {
+      return
+    }
+
+    audioRef.current.volume = volume
+  }
+
+  return { audioRef, play, stop, setVolume }
+}
