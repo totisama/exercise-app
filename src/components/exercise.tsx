a957d1697135420ed01558ee8c3bad269f441259
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-06-06 18:34:16 +0200

    add audio tags and music hook + modify play button

--- src/components/exercise.tsx
+++ src/components/exercise.tsx
@@ -3,12 +3,14 @@
 import Image from 'next/image'
 import dynamic from 'next/dynamic'
 import { CountdownTimer } from '@/components/countdown-timer'
-import { PlayButton } from '@/components/play-button'
+import { ExerciseButton } from '@/components/exercise-button'
 import { ExerciseType } from '@/generated/graphql'
 import { useExercise } from '@/hooks/useExercise'
 import { useIsPlaying } from '@/hooks/useIsPlaying'
 import { type Exercise as ExerciseInterface } from '@/types'
 import { useSpeak } from '@/hooks/useSpeak'
+import { useMusic } from '@/hooks/useMusic'
+import { PlayIcon, SkipIcon } from '@/icons'
 
 // importing it this way to avoid hydration error
 const ReactPlayer = dynamic(async () => await import('react-player/lazy'), {
@@ -20,14 +22,17 @@ interface ExerciseProps {
 }
 
 export const Exercise = ({ exercises }: ExerciseProps) => {
+  const { audioRef, play, setVolume } = useMusic()
+  const { audioRef: audioRefCD, play: playCD } = useMusic()
   const {
     currentSet,
     listeningInstructions,
     finishInstructions,
     currentExercise,
     handleNextExercise,
-  } = useExercise(exercises)
-  const { isPlaying, togglePlaying } = useIsPlaying()
+    nextExercise,
+  } = useExercise(exercises, setVolume)
+  const { isPlaying, togglePlaying } = useIsPlaying(play)
   useSpeak(currentExercise, isPlaying, finishInstructions)
 
   return (
@@ -64,6 +69,7 @@ export const Exercise = ({ exercises }: ExerciseProps) => {
           isPlaying={!listeningInstructions && isPlaying}
           time={currentExercise.duration}
           onFinish={handleNextExercise}
+          playCountdown={playCD}
         />
         {currentExercise.type === ExerciseType.Exercise && (
           <div className='flex justify-center gap-3'>
@@ -78,11 +84,39 @@ export const Exercise = ({ exercises }: ExerciseProps) => {
         )}
       </section>
       {!isPlaying && (
-        <PlayButton
-          isPaused={!isPlaying}
-          togglePause={togglePlaying}
-        />
+        <ExerciseButton
+          bgColor='bg-light-green'
+          onClick={togglePlaying}
+        >
+          <span className='flex gap-1 items-center justify-center'>
+            <PlayIcon className='h-7 w-7 font-bold' />
+            Start
+          </span>
+        </ExerciseButton>
       )}
+      {isPlaying && (
+        <ExerciseButton
+          disabled={listeningInstructions}
+          onClick={nextExercise}
+          bgColor='bg-light-blue'
+        >
+          <span className='flex gap-1 items-center justify-center'>
+            <SkipIcon className='h-7 w-7 font-bold' />
+            Skip exercise
+          </span>
+        </ExerciseButton>
+      )}
+      <audio
+        loop
+        preload='true'
+        src='https://eu-central-1-shared-euc1-02.graphassets.com/clwue0h040dro07w1cfgacud6/clx3fncopxv4h06w4fsxxjs5o?_gl=1*1airq03*_gcl_au*MTg0MTkzMTMzMy4xNzE2ODgzOTY2*_ga*MTY4OTgwNDc1MC4xNzE2ODgzOTY2*_ga_G6FYGSYGZ4*MTcxNzY4NzU2MS4xOC4xLjE3MTc2ODkxMjcuNjAuMC4w'
+        ref={audioRef}
+      ></audio>
+      <audio
+        preload='true'
+        src='https://eu-central-1-shared-euc1-02.graphassets.com/clwue0h040dro07w1cfgacud6/clx3gykfejger07us9jhoaxgr?_gl=1*1xc4hkp*_gcl_au*MTg0MTkzMTMzMy4xNzE2ODgzOTY2*_ga*MTY4OTgwNDc1MC4xNzE2ODgzOTY2*_ga_G6FYGSYGZ4*MTcxNzY5MTE1Ni4xOS4xLjE3MTc2OTExNjEuNTUuMC4w'
+        ref={audioRefCD}
+      ></audio>
     </>
   )
 }
