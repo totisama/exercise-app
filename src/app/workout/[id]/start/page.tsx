947e9c0a52532253705fd1af27a8d2b6511956ca
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-06-06 10:03:31 +0200

    Modify page to display exercise component

--- src/app/workout/[id]/start/page.tsx
+++ src/app/workout/[id]/start/page.tsx
@@ -1,3 +1,21 @@
-export default function StartPage() {
-  return <h1>StartPage</h1>
+import { Exercise } from '@/components/exercise'
+import { sdk } from '@/lib/client'
+
+export default async function StartPage({
+  params: { id },
+}: {
+  params: { id: string }
+}) {
+  const { data } = await sdk.GetWorkoutExercises({ id })
+  const { workout } = data
+
+  if (workout === null || workout === undefined) {
+    return <h1>Workout not found</h1>
+  }
+
+  return (
+    <main className='w-full space-y-1 my-5 px-5 pb-10'>
+      <Exercise exercises={workout.exercises} />
+    </main>
+  )
 }
